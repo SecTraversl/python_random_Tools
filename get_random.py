674acfe295a7e911c4a0c19bc060b2ccc3562acb
Travis Logue <91381575+SecTraversl@users.noreply.github.com>
2022-10-11 16:00:52 -0700

    Refactored the code - 2022-10-11

--- get_random.py
+++ get_random.py
@@ -1,8 +1,24 @@
-# %%
-#######################################
-def get_random(thelist: list, samplesize=10):
-    import random
-    
-    results_list = random.choices(thelist, k=samplesize)
+import random
+
+
+# THE PRELIMINARIES
+
+
+# THE CODE
+def get_random(iterable: object, sample_size=10):
+    """Creates a random selection list from a given iterable.  Defaults to a 10 item list of samples, but this can changed with the 'sample_size' parameter. 
+
+    Args:
+        iterable (object): _description_
+        sample_size (int, optional): _description_. Defaults to 10.
+
+    Returns:
+        list: Returns a list
+    """
+    results_list = random.choices(iterable, k=sample_size)
     return results_list
+
+
+# THE EXECUTION CONTROL
+# if __name__ == '__main__':
 
